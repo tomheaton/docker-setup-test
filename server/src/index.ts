863406ba5a2f6cb7d9241a48a48b8272e94a8971
Thomas Heaton <thomas.m.heaton@gmail.com>
2022-08-06 12:46:11 +0100

    fixes?

--- server/src/index.ts
+++ server/src/index.ts
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 connect(
-    "mongodb://mongodb:27017/test",
+    "mongodb://mongodb:27017/",
     {},
 ).then(async () => {
     console.log("[server] database connected")
